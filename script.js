4d30261547665ae27d8bd20ec0fd9658ddd5f8d0
lbair39 <lilybair24@g.crookcountyschools.org>
2022-02-01 09:54:54 -0800

    finished array repo

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+console.log('hello world');
+
+document.write("<h1>Arrays Repo</h1>")
+
+const pets = ["dogs", "cats", "fish", "hamsters"];
+
+console.log(pets);
+
+pets.push("snakes");
+
+console.log(pets);
+
+console.log(pets.length);
+
+var i = 0;
+
+for(i=0; i<=pets.length; i++){
+    console.log(pets[i]);
+}
+
+document.write(pets)
+
+const classes = ["math", "science", "history", "english"];
+
+console.log(classes)
+
+classes.pop('history')
+
+console.log(classes)
+
